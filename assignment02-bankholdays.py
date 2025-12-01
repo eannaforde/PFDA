fab857b17d9d67b8410b47f6080c90377543f299
eannaforde <159945358+eannaforde@users.noreply.github.com>
2025-12-01 19:51:34 +0000

    assignment02 bank hliday initial commit

---
+++ assignment02-bankholdays.py
@@ -0,0 +1,8 @@
+# assignment 02 bankholidays.py
+# This programme prints the Northern Ireland bank holidays
+
+print("Northern Ireland bank holidays")
+print("-------------------------------")
+
+print("New Years Day - 1 January")
+print("St Patricks Day - 17 March")
